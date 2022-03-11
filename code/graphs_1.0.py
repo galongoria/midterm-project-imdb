b74b79bdc03f97ee99d8bebfeb25982caa57f3d8
ElliottMetzler <e.metzler37@gmail.com>
2022-03-11 15:38:05 +0000

    refactored .py file for graphs

--- code/graphs_1.0.py
+++ code/graphs_1.0.py
@@ -1,82 +1,77 @@
-import os
-import csv
 import pandas as pd
+import numpy as np
+import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-IN_PATH = os.path.join("data", "clean", "imdb_clean.csv")
-
-with open(IN_PATH, "r") as in_file:
-    csv_reader = csv.DictReader(in_file)
-
-    try_ = []
-
-    for row in csv_reader:
-        try_.append(row)
-        
-# print(try_)
-
-status_count = {}
-
-for row in try_:
-
-    num_shares = float(row["GrossRevenue"])
-    status_type = row["Title"] , int(row["ReleaseYear"]) , row["Genres"], int(row["Runtime"]), float(row["IMDBRating"]), int(row["Metascore"])
-    
-    if status_type in status_count:
-        status_count[status_type] = num_shares
-    else:
-        status_count[status_type] = num_shares
-            
-# print(status_count)
+# Constants
+IN_FILE_PATH = os.path.join("data", "clean", "imdb_clean.csv")
+OUT_DIR = "figures"
 
 
-my_list = list(key+(val,) for key,val in status_count.items())
-# print(my_list)
+# Functions
+def save_plot(figure_obj, output_directory, output_file_name):
+    """Function takes in a figure, the output directory and file name and saves the figure"""
+    path = os.path.join(output_directory, output_file_name)
+    figure_obj.savefig(path)
 
-sorted_list = sorted(my_list, key=lambda x: (-x[6], x[1]))
 
-df = pd.DataFrame(sorted_list, columns=["Title", "ReleaseYear", "Genres", "Runtime", "IMDBRating", "Metascore", "GrossRevenue"]).head(50)
+if __name__ == "__main__":
+    os.makedirs(OUT_DIR, exist_ok=True)
+    df = pd.read_csv(IN_FILE_PATH)
 
-sorted_list2 = sorted(my_list, key=lambda x: (-x[4], -x[6]))
+    # First Plot
+    plot1 = (
+        df.groupby("Title")["GrossRevenue"]
+        .mean()
+        .sort_values(ascending=False)[:50]
+        .plot.bar(figsize=(25, 8))
+    )
+    save_plot(plot1.figure, OUT_DIR, "Figure1.png")
 
-df2 = pd.DataFrame(sorted_list2, columns=["Title", "ReleaseYear", "Genres", "Runtime", "IMDBRating", "Metascore", "GrossRevenue"]).head(50)
+    # Second Plot
+    plot2 = (
+        df.groupby("Title")["IMDBRating"]
+        .mean()
+        .sort_values(ascending=False)[:50]
+        .plot.bar(figsize=(25, 8))
+    )
+    save_plot(plot2.figure, OUT_DIR, "Figure2.png")
 
-sorted_list3 = sorted(my_list, key=lambda x: (-x[5], -x[6]))
+    # Third Plot
+    plot3 = (
+        df.groupby("Title")["Metascore"]
+        .mean()
+        .sort_values(ascending=False)[:50]
+        .plot.bar(figsize=(25, 8))
+    )
+    save_plot(plot3.figure, OUT_DIR, "Figure3.png")
 
-df3 = pd.DataFrame(sorted_list3, columns=["Title", "ReleaseYear", "Genres", "Runtime", "IMDBRating", "Metascore", "GrossRevenue"]).head(50)
+    # Fourth Plot
+    plot4 = plt.figure(figsize=(20, 10))
+    plot4 = sns.scatterplot(data=df, x="IMDBRating", y="GrossRevenue")
+    save_plot(plot4.figure, OUT_DIR, "Figure4.png")
 
-sorted_list4 = sorted(my_list, key=lambda x: (-x[3], -x[6]))
+    # Fifth Plot
+    plot5 = plt.figure(figsize=(20, 10))
+    plot5 = sns.scatterplot(data=df, x="Runtime", y="GrossRevenue")
+    plot5.axhline(np.mean(df["GrossRevenue"]), color="green")
+    plot5.axvline(np.mean(df["Runtime"]), color="green")
+    save_plot(plot5.figure, OUT_DIR, "Figure5.png")
 
-df4 = pd.DataFrame(sorted_list3, columns=["Title", "ReleaseYear", "Genres", "Runtime", "IMDBRating", "Metascore", "GrossRevenue"]).head(50)
+    # Sixth Plot
+    plot6 = plt.figure(figsize=(20, 10))
+    plot6 = graph2 = sns.scatterplot(data=df, x="Metascore", y="GrossRevenue")
+    plot6.axhline(np.mean(df["GrossRevenue"]), color="red")
+    plot6.axvline(np.mean(df["Metascore"]), color="red")
+    save_plot(plot6.figure, OUT_DIR, "Figure6.png")
 
-df5 = pd.DataFrame(sorted_list, columns=["Title", "ReleaseYear", "Genres", "Runtime", "IMDBRating", "Metascore", "GrossRevenue"])
+    # Seventh plot
+    plot7 = plt.figure(figsize=(15, 8))
+    plot7 = sns.lineplot(data=df, x="ReleaseYear", y="GrossRevenue")
+    save_plot(plot7.figure, OUT_DIR, "Figure7.png")
 
-df.groupby("Title")["GrossRevenue"].mean().sort_values(ascending=False).plot.bar(figsize=(25, 8))
-
-df2.groupby("Title")["IMDBRating"].mean().sort_values(ascending=False).plot.bar(figsize=(25, 8))
-
-df3.groupby("Title")["Metascore"].mean().sort_values(ascending=False).plot.bar(figsize=(25, 8))
-
-a = df5[['Metascore', 'Runtime', 'GrossRevenue']].mean()
-print(a)
-
-# Revenue by IMDBRating
-plt.figure(figsize=(20,10))
-sns.scatterplot(data=df5, x='IMDBRating', y='GrossRevenue')
-
-plt.figure(figsize=(20,10))
-graph1 = sns.scatterplot(data=df5, x='Runtime', y='GrossRevenue')
-graph1.axhline(124.986511, color = 'green')
-graph1.axvline(116.009321, color = 'green')
-
-plt.figure(figsize=(20,10))
-graph2 = sns.scatterplot(data=df5, x='Metascore', y='GrossRevenue')
-graph2.axhline(124.986511, color = 'red')
-graph2.axvline(59.651798, color = 'red')
-
-plt.figure(figsize=(15,8))
-sns.lineplot(data=df5, x='ReleaseYear', y='GrossRevenue')
-
-plt.figure(figsize=(15,8))
-sns.lineplot(data=df5, x='ReleaseYear', y='Runtime')
+    # Eighth plot
+    plot8 = plt.figure(figsize=(15, 8))
+    plot8 = sns.lineplot(data=df, x="ReleaseYear", y="Runtime")
+    save_plot(plot8.figure, OUT_DIR, "Figure8.png")
