49013ba0a96473af5549fa0806d55c1cd4c54de9
koneeb <koneeb@utexas.edu>
2022-03-14 14:53:10 +0000

    Improved quant_analysis.py.

--- code/quant_analysis.py
+++ code/quant_analysis.py
@@ -10,61 +10,27 @@ SUMMARY_PATH = os.path.join(OUTPUT_DIR, "decade_analysis.csv")
 df = pd.read_csv(IN_PATH)
 
 def decade_summary(out_path):
-    '''Convert summary statistics for each decade to csv'''
-
-    '''Find the minimum and maximum release years to divide data into decades'''
+    '''Group data by decade and convert summary statistics to csv'''   
     
-    year = df["ReleaseYear"]
-    print(year.min())
-    print(year.max())
+    (
+    df.assign(Decade=lambda df: (df["ReleaseYear"]//10)*10)
+    .groupby("Decade")
+    .describe()
+    .to_csv(SUMMARY_PATH)
+    )
 
-    '''Divide data into decades'''
-
-    df_1930 = df[(year//10)*10 == 1930]
-    df_1940 = df[(year//10)*10 == 1940]
-    df_1950 = df[(year//10)*10 == 1950]
-    df_1960 = df[(year//10)*10 == 1960]
-    df_1970 = df[(year//10)*10 == 1970]
-    df_1980 = df[(year//10)*10 == 1980]
-    df_1990 = df[(year//10)*10 == 1990]
-    df_2000 = df[(year//10)*10 == 2000]
-    df_2010 = df[(year//10)*10 == 2010]
-    df_2020 = df[(year//10)*10 == 2020]
-
-    '''Create csv for summary statistics by decade'''
-    
-    df_1930.describe().to_csv(out_path)
-    df_1940.describe().to_csv(out_path, mode ="a")
-    df_1950.describe().to_csv(out_path, mode ="a")
-    df_1960.describe().to_csv(out_path, mode ="a")
-    df_1970.describe().to_csv(out_path, mode ="a")
-    df_1980.describe().to_csv(out_path, mode ="a")
-    df_1990.describe().to_csv(out_path, mode ="a")
-    df_2000.describe().to_csv(out_path, mode ="a")
-    df_2010.describe().to_csv(out_path, mode ="a")
-    df_2020.describe().to_csv(out_path, mode ="a")
 
 def ols_regression(out_path):
-    '''Perform OLS regression of movie revenue by IMBD score and create csv of results'''
+    '''Perform OLS regression of movie revenue by IMBD score and Metascore and create csv'''
 
-    revenue = df["GrossRevenue"]
-    imdb_score = df["IMDBRating"]
-    imdb_score_model = sm.OLS(revenue, sm.add_constant(imdb_score))
-    imdb_score_fit = imdb_score_model.fit()
-    imdb_score_summary = imdb_score_fit.summary()
+    x = df[["IMDBRating", "Metascore", "ReleaseYear"]]
+    y = df["GrossRevenue"]
+    model = sm.OLS(y, sm.add_constant(x))
+    model_fit = model.fit()
+    model_fit_summary = model_fit.summary()
 
-
-    imdb_score_as_html = imdb_score_summary.tables[1].as_html()
-    pd.read_html(imdb_score_as_html, header=0, index_col=0)[0].to_csv(out_path)
-
-    '''Perform OLS regression of movie revenue by Metascore score and create csv of results'''
-    metascore= df["Metascore"]
-    metascore_model = sm.OLS(revenue, sm.add_constant(metascore))
-    metascore_fit = metascore_model.fit()
-    metascore_summary = metascore_fit.summary()
-
-    metascore_as_html = metascore_summary.tables[1].as_html()
-    pd.read_html(metascore_as_html, header=0, index_col=0)[0].to_csv(out_path, mode="a", header=False)
+    model_as_html = model_fit_summary.tables[1].as_html()
+    pd.read_html(model_as_html, header=0, index_col=0)[0].to_csv(OLS_PATH)
 
 
 if __name__ == "__main__":
