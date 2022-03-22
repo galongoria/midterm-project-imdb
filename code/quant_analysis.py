697a0de8eccbdfed6e62191ce6a2f7da8a106479
Kashaf Oneeb <98051441+koneeb@users.noreply.github.com>
2022-03-21 21:55:15 -0500

    Revert "Merge final revisions into main"

--- code/quant_analysis.py
+++ code/quant_analysis.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import os
 import statsmodels.api as sm
-import matplotlib.pyplot as plt
 
 IN_PATH = os.path.join("data", "clean", "imdb_clean.csv")
 OUTPUT_DIR = "quantitative analysis"
-REVENUE_IMDB_OLS_PATH = os.path.join(OUTPUT_DIR, "revenue_imdb_ols_regression.png")
-REVENUE_META_OLS_PATH = os.path.join(OUTPUT_DIR, "revenue_meta_ols_regression.png")
-IMDB_OLS_PATH = os.path.join(OUTPUT_DIR, "imdb_ols_regression.png")
-METASCORE_OLS_PATH = os.path.join(OUTPUT_DIR, "metascore_ols_regression.png")
+REVENUE_OLS_PATH = os.path.join(OUTPUT_DIR, "revenue_ols_regression.csv")
+IMDB_OLS_PATH = os.path.join(OUTPUT_DIR, "imdb_ols_regression.csv")
+METASCORE_OLS_PATH = os.path.join(OUTPUT_DIR, "metascore_ols_regression.csv")
 SUMMARY_PATH = os.path.join(OUTPUT_DIR, "decade_analysis.csv")
 
 df = pd.read_csv(IN_PATH)
@@ -27,87 +25,50 @@ def decade_summary(out_path):
     )
 
 
-def revenue_imdb_ols_regression(out_path):
-    '''Perform OLS regression of movie Revenue on IMBD Rating and genre dummies, and create png'''
+def revenue_ols_regression(out_path):
+    '''Perform OLS regression of movie Revenue on IMBD Rating, Metascore, Release Year, and genre dummies and create csv'''
     
-    x_cols = ["IMDBRating"]
+    x_cols = ["IMDBRating", "Metascore", "ReleaseYear"]
     for col in dummy_cols:
         x_cols.append(col)
 
     x = df[x_cols]
     y = df["GrossRevenue"]
-    
-    model = sm.OLS(y, sm.add_constant(x)).fit()
-    model_summary = model.summary()
-    
-    
-    fig, ax = plt.subplots(figsize=(12, 7))
-    
-    ax.text(0.01, 0.05, str(model_summary), {"fontsize": 10}, fontproperties = "monospace")
-    ax.axis("off")
-    plt.tight_layout()
-    fig.savefig(out_path)
-    
-    
-def revenue_meta_ols_regression(out_path):
-    '''Perform OLS regression of movie Revenue on Metascore and genre dummies, and create png'''
-    
-    x_cols = ["Metascore"]
-    for col in dummy_cols:
-        x_cols.append(col)
+    model = sm.OLS(y, sm.add_constant(x))
+    model_fit = model.fit()
+    model_fit_summary = model_fit.summary()
 
-    x = df[x_cols]
-    y = df["GrossRevenue"]
-    
-    model = sm.OLS(y, sm.add_constant(x)).fit()
-    model_summary = model.summary()
-    
-    
-    fig1, ax1 = plt.subplots(figsize=(12, 7))
-    
-    ax1.text(0.01, 0.05, str(model_summary), {"fontsize": 10}, fontproperties = "monospace")
-    ax1.axis("off")
-    plt.tight_layout()
-    fig1.savefig(out_path)
-    
+    model_as_html = model_fit_summary.tables[1].as_html()
+    pd.read_html(model_as_html, header=0, index_col=0)[0].to_csv(out_path)
+
 def imdb_ols_regression(out_path):
-    '''Perform OLS regression of IMBD Rating on genre dummies and create png'''
+    '''Perform OLS regression of IMBD Rating on genre dummies and create csv'''
     
     x = df[dummy_cols]
     y = df["IMDBRating"]
+    model = sm.OLS(y, sm.add_constant(x))
+    model_fit = model.fit()
+    model_fit_summary = model_fit.summary()
 
-    model = sm.OLS(y, sm.add_constant(x)).fit()
-    model_summary = model.summary()
-    
-    
-    fig2, ax2 = plt.subplots(figsize=(12, 7))
-    
-    ax2.text(0.01, 0.05, str(model_summary), {"fontsize": 10}, fontproperties = "monospace")
-    ax2.axis("off")
-    plt.tight_layout()
-    fig2.savefig(out_path)
+    model_as_html = model_fit_summary.tables[1].as_html()
+    pd.read_html(model_as_html, header=0, index_col=0)[0].to_csv(out_path)
     
 def metascore_ols_regression(out_path):
-    '''Perform OLS regression of Metascore on genre dummies and create png'''
+    '''Perform OLS regression of Metascore on genre dummies and create csv'''
     
     x = df[dummy_cols]
     y = df["Metascore"]
-    
-    model = sm.OLS(y, sm.add_constant(x)).fit()
-    model_summary = model.summary()
-    
-    
-    fig3, ax3 = plt.subplots(figsize=(12, 7))
-    
-    ax3.text(0.01, 0.05, str(model_summary), {"fontsize": 10}, fontproperties = "monospace")
-    ax3.axis("off")
-    plt.tight_layout()
-    fig3.savefig(out_path)
-    
+    model = sm.OLS(y, sm.add_constant(x))
+    model_fit = model.fit()
+    model_fit_summary = model_fit.summary()
+
+    model_as_html = model_fit_summary.tables[1].as_html()
+    pd.read_html(model_as_html, header=0, index_col=0)[0].to_csv(out_path)
+
+
 if __name__ == "__main__":
     os.makedirs(OUTPUT_DIR, exist_ok=True)
     decade_summary(SUMMARY_PATH)
-    revenue_imdb_ols_regression(REVENUE_IMDB_OLS_PATH)
-    revenue_meta_ols_regression(REVENUE_META_OLS_PATH)
+    revenue_ols_regression(REVENUE_OLS_PATH)
     imdb_ols_regression(IMDB_OLS_PATH)
     metascore_ols_regression(METASCORE_OLS_PATH)
